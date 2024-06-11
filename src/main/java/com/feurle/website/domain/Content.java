7d8a14d7c0dd84fbba8b2b9725bc3be60805fcf9
Daniel Feurle <daniel@feurle.com>
2024-06-12 01:06:02 +0200

    add prometheus base

--- src/main/java/com/feurle/website/domain/Content.java
+++ src/main/java/com/feurle/website/domain/Content.java
@@ -1,7 +1,6 @@
-package com.feurle.website.model;
+package com.feurle.website.domain;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
